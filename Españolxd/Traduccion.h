6def2b1a786d5daedf39ba5f7f66b127eda4886e
Luis González <lluis.gora@gmail.com>
2024-01-12 10:29:40 +0100

    Code to test

---
+++ Españolxd/Traduccion.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+
+#define imprimir std::cout
+#define leer std::cin
+#define findelafrase std::endl
+#define unomas ++
+#define unomenos --
+#define Y &&
+#define O ||
+#define principal main
+#define devolver return
+#define si if
+#define sino else
+#define mientras while
+#define para for
+#define hacer do
+#define verdadero true
+#define falso false
+#define entero int
+#define flotante float
+#define doble double
+#define sinsimbolo unsigned
+#define caracter char
+#define cadena std::string
+#define cambiar switch
+#define caso case
+#define romper break
+#define continuar continue
+#define tamaño_de sizeof
+#define nuevo new
+#define eliminar delete
+#define nulo nullptr
+#define clase class
+#define estructura struct
---
+++ Españolxd/main.cpp
@@ -0,0 +1,21 @@
+#include "Traduccion.h"
+
+entero principal()
+{
+	entero cinco = 5;
+
+	para(entero numero = 0; numero < 10; numero unomas)
+	{
+
+		si(cinco == 5 Y numero == cinco)
+		{
+			imprimir << "cinco es 5" << findelafrase;
+		}
+		sino
+		{
+			imprimir << "cinco NO es 5" << findelafrase;
+		}
+	}
+
+	devolver 0;
+}
